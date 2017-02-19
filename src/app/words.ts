dd79f0b4d1d6455e2208fea973ff1ccd3e1f937e
nick raphael <nick.raphael@gmail.com>
2017-02-19 22:14:55 +1100

    deploy?

--- src/app/words.ts
+++ src/app/words.ts
@@ -8,7 +8,7 @@ export class Words {
     static allWords: IWord[] = [
         { "colour": "red", "word": "no" },
         { "colour": "red", "word": "yes" },
-        { "colour": "blue", "word": "hi" },
+        { "colour": "blue", "word": "hit" },
         { "colour": "blue", "word": "yo" },
         { "colour": "blue", "word": "to" }
     ]
